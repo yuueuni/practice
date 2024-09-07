b7604fa4604be75f4e7d08226c35d37c7cd66c91
yuueuni <yuueuni@gmail.com>
2024-09-07 20:28:10 +0900

    java springboot init commit

---
+++ java-springboot/src/main/java/com/yoon/java_springboot/JavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.java_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootApplication.class, args);
+	}
+
+}
