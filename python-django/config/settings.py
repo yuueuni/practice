7504cc1a5075fc8333f89ecdae7642dc645ac5c5
yuueuni <yuueuni@gmail.com>
2024-09-07 20:06:26 +0900

    프로젝트 설정 수정
    - sercret key 설정
    - requirements.txt 추가

--- python-django/config/settings.py
+++ python-django/config/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import base64
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+b64key = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = base64.b64decode(b64key).decode('utf-8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +45,9 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     # openapi
     'drf_spectacular',
+
+    # Custom apps
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
