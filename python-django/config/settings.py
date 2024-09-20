91ef4209263780918474b07b81dd48b86900b8b5
yuueuni <yuueuni@gmail.com>
2024-09-21 02:27:18 +0900

    user models, serializer 추가

--- python-django/config/settings.py
+++ python-django/config/settings.py
@@ -131,4 +131,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.User'
